780708623a84830f96e60d3a40c2b7ac22fc4212
l3miage-benaisil <benaissailian@outlook.fr>
2023-03-12 20:28:06 +0100

    clean import

--- data/src/test/java/fr/uga/l3miage/library/data/repo/Fixtures.java
+++ data/src/test/java/fr/uga/l3miage/library/data/repo/Fixtures.java
@@ -5,7 +5,6 @@ import fr.uga.l3miage.library.data.domain.*;
 
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
